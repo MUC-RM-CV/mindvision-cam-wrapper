98e4c0af4dd0e211de32c3a846c381a560dfe81c
henry_23 <henry.maoht@outlook.com>
2021-05-20 06:09:04 +0800

    add: VideoInput: cv::VideoCapture wrapper

---
+++ include/image/VideoInput.h
@@ -0,0 +1,23 @@
+#ifndef VIDEO_INPUT_H
+#define VIDEO_INPUT_H
+
+#include "ImageInput.h"
+
+#include <iostream>
+#include <string>
+
+class VideoInput : public ImageInput {
+private:
+    cv::Mat tmp;
+    cv::VideoCapture origin;
+    void initUSBCamera();
+
+public:
+    VideoInput();
+    VideoInput(const std::string & fileName);
+    ~VideoInput();
+    bool    init() final;
+    cv::Mat read() final;
+};
+
+#endif
---
+++ src/image/VideoInput.cpp
@@ -0,0 +1,50 @@
+#include "image/VideoInput.h"
+
+VideoInput::VideoInput()
+{
+    opened = init();
+}
+
+VideoInput::VideoInput(const std::string & fileName)
+{
+    origin.open(fileName);
+    if (origin.isOpened()) {
+        std::cout << "Input video '" << fileName << "' opened successfully." << std::endl;
+        opened = true;
+    }
+    else {
+        std::cout << "Failed to open '" << fileName << "' Fall back to USB Camera." << std::endl;
+        opened = initUSBCamera();
+    }
+}
+
+bool VideoInput::init() { 
+    return initUSBCamera(); 
+}
+
+VideoInput::~VideoInput()
+{
+    origin.release();
+    std::cout << "VideoInput Released!\n";
+}
+
+bool VideoInput::initUSBCamera()
+{
+    std::cout << "Try using USB camera. " << std::endl;
+    origin.open(0);
+    if (origin.isOpened()) {
+        std::cout << "USB Camera opened successfully. " << std::endl;
+        return true;
+    }
+    else {
+        std::cout << "Failed to open USB Camera!" << std::endl;
+        return false;
+    }
+}
+
+cv::Mat VideoInput::read(cv::Mat& input)
+{
+    origin >> tmp;
+    cv::resize(tmp, tmp, imgResolution);
+    return tmp;
+}
